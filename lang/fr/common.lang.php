cc65507fb5c375fe8992ef2b1f4650801cf6acf4
Pascal VINEY <viney@novius.fr>
2013-11-06 15:55:17 +0100

    Added french language

---
+++ lang/fr/common.lang.php
@@ -0,0 +1,23 @@
+<?php
+
+// Generated on 19/08/2013 18:12:14
+
+// 1 out of 1 messages are translated (100%).
+// 4 out of 4 words are translated (100%).
+
+return array(
+    'Add another online media'                      => '+',
+    'Width'                                         => 'Largeur',
+    'Height'                                        => 'Hauteur',
+    'Update this media'                             => 'Modifier ce média internet',
+    'Select a folder from the list'                 => 'Selectionnez un dossier dans la liste',
+    'Pick an internet media'                        => 'Selectionner un média internet',
+    'Pick another internet media'                   => 'Selectionner un autre média internet',
+    'No internet media'                             => 'Pas de média internet',
+    'internet media'                                => 'média internet',
+    'internet medias'                               => 'médias internet',
+    'Showing {{x}} internet medias out of {{y}}'    => '{{x}} média internet affiché sur {{y}}',
+    'Showing 1 internet media'                      => '1 média internet affiché',
+    'No internet medias'                            => 'Pas de média internet',
+    'Show all internet medias'                      => 'Afficher tous les médias internet',
+);
